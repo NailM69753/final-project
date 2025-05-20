5928d8bef32fed7b72702a2ac93149f9c7927337
NailM69753 <nmurghu1@stu.vistula.edu.pl>
2025-05-20 22:58:05 +0200

    Add files via upload

---
+++ bmwm3/src/test/java/com/example/bmwm3/Bmwm3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bmwm3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bmwm3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
